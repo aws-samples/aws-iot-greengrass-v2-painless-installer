7af9be42eee38f07480832245062bcfa9b5afd3c
lautip <lautip@amazon.com>
2022-08-23 18:19:21 +0200

    Reorganise the files and names for this project.

--- cdk/greengrass_installer_stack.py
+++ cdk/greengrass_installer_stack.py
@@ -5,7 +5,8 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class CdkRootStack(Stack):
+
+class GreengrassInstallerStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
--- unittest/test_cdk_root_stack.py
+++ unittest/test_cdk_root_stack.py
@@ -1,13 +1,14 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from cdk_root.cdk_root_stack import CdkRootStack
+from cdk.greengrass_installer_stack import GreengrassInstallerStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in cdk_root/cdk_root_stack.py
+# resource in cdk_root/greengrass_installer_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = CdkRootStack(app, "cdk-root")
+    stack = GreengrassInstallerStack(app, "cdk-root")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
